9a39973f46be43b32f5cef3bdf3cb932a4e33ab2
KRISH <addresskrish@gmail.com>
2025-07-03 04:43:46 +0530

    updated

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,24 +1,33 @@
+import { useState } from "react"
 import meme from "../assets/meme.png"
 
 function Main() {
+
+    const [text, setText] = useState({topText: "hello", bottomText: "world", img: meme})
+
+    const handleChange = (event) => {
+        const {value, name} = event.currentTarget
+        setText(prevText => ({...prevText, [name]: value}))
+    }
+
     return (
         <>
         <main>
-            <form action="">
+            <div className="form">
                 <label htmlFor="topText">Top text
-                    <input type="text" placeholder="Shut up" name="topText" id="topText"/>
+                    <input type="text" placeholder="Shut up" name="topText" id="topText" onChange={handleChange}/>
                 </label>
 
                 <label htmlFor="bottomText">Bottom Text
-                    <input type="text" placeholder="And take my money" name="bottomText" id="bottomText"/>
+                    <input type="text" placeholder="And take my money" name="bottomText" id="bottomText" onChange={handleChange}/>
                 </label>
 
                 <button>Get a new meme image</button>
-            </form>
+            </div>
             <div className="meme">
-                <img src={meme} alt="meme-img" className="meme-img"/>
-                <span className="memeTopText">hello</span>
-                <span className="memeBottomText">world</span>
+                <img src={text.img} alt="meme-img" className="meme-img"/>
+                <span className="memeTopText">{text.topText}</span>
+                <span className="memeBottomText">{text.bottomText}</span>
             </div>
         </main>
         </>
